9ceef1d6392a2241359114312b9a34ab0248582a
Victor Hugo <vhaporfiro@gmail.com>
2025-04-06 09:24:14 -0300

    Inicio do projeto de gerenciamento de lista de tarefas.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module br.com.vhaporfiro.gerenciamentodetarefas {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens br.com.vhaporfiro.gerenciamentodetarefas to javafx.fxml;
+    exports br.com.vhaporfiro.gerenciamentodetarefas;
+}
